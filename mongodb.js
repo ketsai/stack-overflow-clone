a70b3c1c2f8a6192bcd57acd8154f36b29ec9c34
ketsai <38197309+ketsai@users.noreply.github.com>
2019-04-19 22:27:54 -0400

    Update mongodb.js

--- mongodb.js
+++ mongodb.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 // mongodb://username:password@serverip:27017/dbname
-var mongoDB = 'mongodb://cse356:cse356@130.245.170.45:27017/stack-overflow';
+var mongoDB = 'mongodb://130.245.170.45:27017/stack-overflow';
 var options = {
     useNewUrlParser: true 
   }
