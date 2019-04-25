6bd40ec954bbc7475a547d82d4886284956e52fd
TZHENG-PC\Tina <tina.zheng.1@stonybrook.edu>
2019-04-24 22:41:48 -0400

    Single mongo not sharded

--- mongodb.js
+++ mongodb.js
@@ -1,7 +1,8 @@
 var mongoose = require('mongoose');
 // mongodb://username:password@serverip:27017/dbname
 
-var mongoDB = 'mongodb://192.168.193.150:27017,192.168.193.248:27017/stack-overflow';
+var mongoDB = 'mongodb://130.245.169.123:27017/stack-overflow';
+//var mongoDB = 'mongodb://192.168.193.150:27017,192.168.193.248:27017/stack-overflow';
 var options = {
     useNewUrlParser: true 
   }
@@ -14,4 +15,4 @@ db.on('error', console.error.bind(console, 'connection error:'));
 
 db.once('open', function() {
     console.log("Connected to Mongoose");
-});
+};
